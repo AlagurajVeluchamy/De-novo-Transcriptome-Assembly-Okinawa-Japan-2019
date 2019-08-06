0ed3c3331f582bb7d0231e946ffd1f75f0fa7187
Alaguraj Veluchamy <alaguraj.v@gmail.com>
2019-08-07 00:28:18 +0900

    Update 01_connectToServer.sh

--- 00_Linux_cmds/01_connectToServer.sh
+++ 00_Linux_cmds/01_connectToServer.sh
@@ -1,17 +1,25 @@
-ssh username@universityservername.jp
-exit
-mkdir mynamedir_rnaseq
-ls
-cd foldername
-pwd
-cd ..
-copy fastq
-wget genomefile
-wget gtffile
-mv filename filename1
-cut -f filename
-grep
-wc 
-head
-tail
-more
+putty for windows
+ssh sci-RNAseq01@pjc.lab.u-ryukyu.ac.jp
+Purpose	User ID	Password
+lecturer	sci-RNAseq-lecture	y7gq3upi
+student	sci-RNAseq01	k6pgwyei
+student	sci-RNAseq02	c5neiyr7
+student	sci-RNAseq03	z3qmf2vg
+student	sci-RNAseq04	k89qxtwb
+student	sci-RNAseq05	u5rk29fg
+student	sci-RNAseq06	k24aw78p
+student	sci-RNAseq07	u58qdi6n
+student	sci-RNAseq08	u5m2dz9v
+student	sci-RNAseq09	q2zmu6a5
+student	sci-RNAseq10	x2va53yb
+student	sci-RNAseq11	q6h7y2nb
+student	sci-RNAseq12	m7ai5cwb
+		
+ Datain /mnt/sci-RNAseq	
+
+
+
+
+
+
+
