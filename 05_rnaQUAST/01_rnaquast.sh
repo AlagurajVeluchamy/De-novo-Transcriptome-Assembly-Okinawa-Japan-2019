f1437a63aa52d6776f1730b366698b0812461a75
Alaguraj Veluchamy <alaguraj.v@gmail.com>
2019-08-07 07:27:27 +0900

    Update 01_rnaquast.sh

--- 05_rnaQUAST/01_rnaquast.sh
+++ 05_rnaQUAST/01_rnaquast.sh
@@ -1,1 +1,1 @@
-rnaquast -h
+python /mnt/sci-RNAseq/progs/rnaQUAST-1.5.2/rnaQUAST.py --transcripts hard_filtered_transcripts.fasta --busco_lineage /mnt/sci-RNAseq/buscodta/protists_ensembl -o resbusco
